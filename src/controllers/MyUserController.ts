a900cd95ab7087072ca4f7821aa50ab517122c10
ankit <ankitkarapurkar444@gmail.com>
2025-02-04 01:13:46 -0800

    food backend

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -1,37 +1,64 @@
 import { Request, Response } from "express";
-import User from "../models/user"
-import jwt from "jsonwebtoken";
-export const UserLogin = async (req: Request, res: Response) => {
+import User from "../models/user";
+
+const getCurrentUser = async (req: Request, res: Response) => {
   try {
-    const { email, password, firstname, lastname } = req.body;
-    let user = await User.findOne({ email });
-
-    if (user) {
-
-      return res.status(200).json({ message: "Login successful", user });
+    const currentUser = await User.findOne({ _id: req.userId });
+    if (!currentUser) {
+      return res.status(404).json({ message: "User not found" });
     }
 
-    user = new User({
-      email,
-      password,
-      firstname,
-      lastname,
-    });
+    res.json(currentUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+const createCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const { auth0Id } = req.body;
+    const existingUser = await User.findOne({ auth0Id });
+
+    if (existingUser) {
+      return res.status(200).send();
+    }
+
+    const newUser = new User(req.body);
+    await newUser.save();
+
+    res.status(201).json(newUser.toObject());
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error creating user" });
+  }
+};
+
+const updateCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const { name, addressLine1, country, city } = req.body;
+    const user = await User.findById(req.userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.name = name;
+    user.addressLine1 = addressLine1;
+    user.city = city;
+    user.country = country;
 
     await user.save();
-    
-    const token = jwt.sign(
-        { UserID: user.id },
-        process.env.JWT_SECRET_TOKEN as string, 
-        { expiresIn: "1h" } 
-      );
-      res.cookie("auth_token", token, {
-        httpOnly: true,  
-        secure: process.env.NODE_ENV === "production",  
-        maxAge: 24 * 60 * 60 * 1000,  
-      });
-   return  res.sendStatus(200);
+
+    res.send(user);
   } catch (error) {
-   return  res.status(500).json({ message: "Internal server error", error });
+    console.log(error);
+    res.status(500).json({ message: "Error updating user" });
   }
+};
+
+export default {
+  getCurrentUser,
+  createCurrentUser,
+  updateCurrentUser,
 };
